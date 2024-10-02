dd10f1567680191cfe9b9096170396584eb14e5e
levgodcompany <levgodcompany@gmail.com>
2024-10-02 20:31:04 -0300

    edit

--- src/modules/ClassOnlive/ClassOnlive.routes.ts
+++ src/modules/ClassOnlive/ClassOnlive.routes.ts
@@ -9,19 +9,19 @@ import {
 const router = Router();
 
 router.use(authenticate);
+router.use(authorizeStudentAndTeacher);
+router.get("/", ClassOnliveController.findAll);
+router.get("/:idClassOnlive", authorizeStudentAndTeacher, ClassOnliveController.findOne);
+
+router.get(
+  "/cohort/class-onlive/:idCohort",
+  ClassOnliveController.findAllClassOnLive
+);
 router.use(authorizeTeacher);
 
 router.post("/", ClassOnliveController.create);
 router.put("/:idClassOnlive", ClassOnliveController.update);
 router.delete("/:idClassOnlive", ClassOnliveController.delete);
 
-router.use(authorizeStudentAndTeacher);
-router.get("/", ClassOnliveController.findAll);
-router.get("/:idClassOnlive", ClassOnliveController.findOne);
-
-router.get(
-  "/cohort/class-onlive/:idCohort",
-  ClassOnliveController.findAllClassOnLive
-);
 
 export default router;
